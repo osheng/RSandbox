1d030eaad691b4bf80f3c8f1b1ac778f8b5c4ba4
osheng <osheng@gmail.com>
2020-02-27 16:45:39 -0500

    partway through 6.3.21 in EnR

--- EnRChap6.R
+++ EnRChap6.R
@@ -1,6 +1,6 @@
 # Problems from Evans and Rosenthal Chapter 6
 
-
+# Question 6.3.19
 # Since mu and sigma are unknown, we compute the length with the t-distribution.
 question6_3_19=function(s,g){
   # compute the length of a gamma confidence interval for n samples 
@@ -12,3 +12,18 @@ question6_3_19=function(s,g){
 }
 question6_3_19(5,0.95)
 
+
+# Question 6.3.21: This question require package Rlab for rbern.
+compute_bernCI=function(sample_data,g){
+  mu = mean(sample_data)
+  fisher_info=length(sample_data)*(1/(mu*(1-mu)))
+  width = qnorm((1+g)/2)*sqrt(1/fisher_info)
+  return(c(mu-width,mu+width))
+}
+ 
+question6_3_21=function(num_samples, sample_size, bparam, g){
+  
+  replicate(num_samples, rbern(sample_size,bparam))
+  
+}
+question6_3_21(10^3, 5, 0.5, 0.95)
