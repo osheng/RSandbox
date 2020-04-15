4ffaebdcb1bfdafbacbf5aa691ef15712b7c677f
osheng <osheng@gmail.com>
2020-04-15 14:23:07 -0400

    wrote LSA.R
    LSA.R plots least squared polynomial approximations.

---
+++ LSA.R
@@ -0,0 +1,26 @@
+LSA=function(x,y,n=1){
+  # Returns a vector of coefficients for least squared polynomial approximation
+  # Note the first value is the y-intercept, the last is the x^n coefficient
+  # @param x
+  # @param y
+  
+  l=length(x)
+  A = matrix(1,nrow=l,ncol=1)
+  for (i in seq(1,n)){
+    temp =matrix(x^i,nrow=l,ncol=1)
+    A = cbind(temp,A) 
+  }
+  return(c(solve(t(A)%*%A) %*% t(A) %*% y))
+} 
+
+LSAplot=function(x,y,n=1,col="red"){
+  # Plots the least squares polynomial degree n approximation
+  px = seq(min(x),max(x),0.1)
+  py = integer(length(px))
+  c = LSA(x,y,n)
+  for (i in seq(0,n)){
+    py = py + c[n + 1 - i] * px^(i)
+  }
+  plot(x,y)
+  lines(px,py,col=col)
+}
